543ea1b2289360a29989ada0d2957e08f83ace3f
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-17 23:22:08 -0400

    setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 print("Secret Key = ", app.config['SECRET_KEY'])
+print("Database URL = ", app.config['SQLALCHEMY_DATABASE_URI'])
 
-from app import routes
+from app import routes, models
