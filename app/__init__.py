c1103ba2fca82fdeda89fd53b0ed67a336565e38
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-21 11:24:56 -0400

    added funtion importBooks to routes

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# from rq import Queue
+# from rq.job import Job
+# from worker import conn
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# q = Queue(connection=conn)
 
 print("Secret Key = ", app.config['SECRET_KEY'])
 print("Database URL = ", app.config['SQLALCHEMY_DATABASE_URI'])
--- app/routes.py
+++ app/routes.py
@@ -85,5 +85,38 @@ def search():
 def review():
     # """Review a Book""
     return "Review a Book"
+    
+@app.route("/importBooks", methods=["GET", "POST"])
+def importBooks():
+    conString =  os.getenv("DATABASE_URL")
+
+    try:
+        connection = psycopg2.connect(conString)
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print ( connection.get_dsn_parameters(),"\n")
+
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record,"\n")
+
+        with open('books.txt', 'r') as f:
+            # Notice that we don't need the 'txt' module.
+            next(f) # Skip the header row.
+            cursor.copy_from(f, 'books', columns=['isbn','title','author','year'])
+
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+            if(connection):
+
+                cursor.close()
+                connection.commit()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
 
 
