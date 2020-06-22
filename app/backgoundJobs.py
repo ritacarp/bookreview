d2bee22d7d62b498b092d94f2735abf3fdd0d112
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-22 17:53:04 -0400

    trying to run background jobs

--- app/backgoundJobs.py
+++ app/backgoundJobs.py
@@ -4,7 +4,7 @@ from app.helpers import foo
 if __name__ == '__main__':
     
     result = foo()
-    print("\nfunction foo with default arguments returned a count of ", result)
+    print("\nBackgound Jobs:  function foo with default arguments returned a count of ", result)
     result = foo(1,20)
-    print("\nfunction foo with arguments start=1, end=20 returned a count of ", result)
+    print("\nBackgound Jobs:  function foo with arguments start=1, end=20 returned a count of ", result)
     
--- createRQ.py
+++ createRQ.py
@@ -5,4 +5,4 @@ from worker import conn
 q = Queue(connection=conn)
 #result = foo(15,40)
 result = q.enqueue(foo, 225,250)
-print("\nIn calling program createRQ:  function foo with arguments start=25, end=55 returned a count of ", result)
+print("\nIn calling program createRQ:  function foo with arguments start=225, end=250 returned a count of ", result)
