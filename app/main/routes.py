bb04f536cc6e73dfef56137a89e021337b587665
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-23 20:24:31 -0400

    trying to launch background task

--- app/main/routes.py
+++ app/main/routes.py
@@ -76,7 +76,7 @@ def index():
 @bp.route("/launchTask/<taskName>", methods=["GET", "POST"])
 def launchTask(taskName=""):
     if taskName:
-        current_app.task_queue.enqueue(('app.tasks.' + taskName)
+        current_app.task_queue.enqueue('app.tasks.' + taskName)
         return f"Task {taskName} has been successfully submitted!"
     else:
         return "Please provide a task name (/launchTask/<task_name>)"
