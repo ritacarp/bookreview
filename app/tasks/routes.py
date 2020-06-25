3feb477dbd1fcdfc81c2e468b06b4aa996afd131
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-25 14:11:59 -0400

    Trying to update goodreads book id

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -20,7 +20,7 @@ def launchTask(taskName="", args=""):
     # https://python-rq.org/
     if taskName:
         if args:
-            current_app.task_queue.enqueue('app.tasks.tasks.' + taskName, args)
+            current_app.task_queue.enqueue('app.tasks.tasks.' + taskName, args, job_timeout='1h')
             return f"Task {taskName} has been successfully submitted with arguments {args}!"
         else:
             current_app.task_queue.enqueue('app.tasks.tasks.' + taskName)
