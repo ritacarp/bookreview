cc75f90480d985a5935c51f0d202aa9535c6ffa6
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-06-25 17:39:23 -0400

    Trying to update goodreads book id

--- app/main/helpers.py
+++ app/main/helpers.py
@@ -26,26 +26,47 @@ def grLookupByID(grID):
         #topLevelNodes = root.findall(".")
         #bookNodes = root.findall("./book")
         
-        bookIDNode = root.findall("./book/id")
-        grBookID = bookIDNode[0].text
+        try:
+           bookIDNode = root.findall("./book/id")
+           grBookID = bookIDNode[0].text
+        except:
+           grBookID = ""
         print("grBookID = ", grBookID)
         
-        isbnNode = root.findall("./book/isbn")
-        isbn = isbnNode[0].text
-        if isbn:
-            isbn.strip()
+        try:
+           isbnNode = root.findall("./book/isbn")
+           isbn = isbnNode[0].text
+           if isbn:
+               isbn.strip()
+        except:
+            isbn = ""
         print("isbn = ", isbn)
-        
-        asinNode = root.findall("./book/asin")
-        asin = asinNode[0].text
-        if asin:
-            asin.strip()
+
+        try:
+           isbn13Node = root.findall("./book/isbn13")
+           isbn13 = isbnNode13[0].text
+           if isbn13:
+               isbn13.strip()
+        except:
+            isbn13 = ""
+        print("isbn13 = ", isbn)
+
+        try:
+            asinNode = root.findall("./book/asin")
+            asin = asinNode[0].text
+            if asin:
+                asin.strip()
+        except:
+            asin = ""
         print("asin = ", asin)
 
-        kindle_asinNode = root.findall("./book/kindle_asin")
-        kindle_asin = kindle_asinNode[0].text
-        if kindle_asin:
-            kindle_asin.strip()
+        try:
+            kindle_asinNode = root.findall("./book/kindle_asin")
+            kindle_asin = kindle_asinNode[0].text
+            if kindle_asin:
+                kindle_asin.strip()
+        except:
+            kindle_asin = ""
         print("kindle_asin = ", kindle_asin)
         
         bookID = asin
@@ -55,47 +76,120 @@ def grLookupByID(grID):
             bookID = isbn
         print("bookID = ", bookID)
 
+        try:
+            titleNode = root.findall("./book/title")
+            title = titleNode[0].text
+        except:
+            title = ""
+        print("title = ", title)
         
-        titleNode = root.findall("./book/title")
-        title = titleNode[0].text
-        # print("title = ", title)
-        
-        descriptionNode = root.findall("./book/description")
-        description = "<h6><small>" + descriptionNode[0].text + "</small></h6>"
-        # print("\n\ndescription = ", description)
+        try:
+            descriptionNode = root.findall("./book/description")
+            description = "<h6><small>" + descriptionNode[0].text + "</small></h6>"
+        except:
+            description = ""
+        print("\n\ndescription = ", description)
 
-        image_urlNode = root.findall("./book/image_url")
-        image_url = image_urlNode[0].text
+        try:
+            image_urlNode = root.findall("./book/image_url")
+            image_url = image_urlNode[0].text
+        except:
+            image_url = ""
         # print("\n\nimage_url = ", image_url)
 
-        small_image_urlNode = root.findall("./book/small_image_url")
-        small_image_url = small_image_urlNode[0].text
+        try:
+            small_image_urlNode = root.findall("./book/small_image_url")
+            small_image_url = small_image_urlNode[0].text
+        except:
+            small_image_url = ""
         # print("\n\nsmall_image_url = ", small_image_url)
+
+        try:
+            average_ratingNode = root.findall("./book/average_rating")
+            average_rating = average_ratingNode[0].text
+        except:
+            average_rating = 0
+        print("average_rating = ", average_rating)
+
+        bookStars = {}
+        try:
+            rating_distNode = root.findall("./book/work/rating_dist")
+            rating_dist = rating_distNode[0].text
+            starsDistribution = rating_dist.split("|")
+            for starsList in starsDistribution:
+                stars = starsList.split(":")
+                bookStars[stars[0]] = stars[1]
+        except:
+            rating_dist = ""
+        print("rating_dist = ", rating_dist)
+
+        stars_1 = bookStars.get('1', 0)
+        stars_2 = bookStars.get('2', 0)
+        stars_3 = bookStars.get('3', 0)
+        stars_4 = bookStars.get('4', 0)
+        stars_5 = bookStars.get('5', 0)
+        
+        print(f"5 stars = {stars_5}")
+        print(f"4 stars = {stars_4}")
+        print(f"3 stars = {stars_3}")
+        print(f"2 stars = {stars_2}")
+        print(f"1 stars = {stars_1}")
+        print("\n")
         
         allAuthors = ""
-        authorsNodes = root.findall("./book/authors/author/name")
-        i = 0
-        for nodes in authorsNodes:
-            if allAuthors != "":
-                allAuthors = allAuthors + "|"
-            authorName = authorsNodes[i].text
-            allAuthors = allAuthors + authorName
-            i += 1
+        try:
+            authorsNodes = root.findall("./book/authors/author/name")
+            i = 0
+            for nodes in authorsNodes:
+                if allAuthors != "":
+                    allAuthors = allAuthors + "|"
+                authorName = authorsNodes[i].text
+                allAuthors = allAuthors + authorName
+                i += 1
+        except:
+            allAuthors = ""
         #print("\n\nallAuthors = ", allAuthors)
+        
+        try:
+            ratings_countNode = root.findall("./book/work/ratings_count")
+            ratings_count = ratings_countNode[0].text
+        except:
+            ratings_count = 0
+        print("ratings_count = ", ratings_count)
+        
+        try:
+            text_reviews_countNode = root.findall("./book/work/text_reviews_count")
+            reviews_count = text_reviews_countNode[0].text
+        except:
+            reviews_count = 0
+        print("reviews_count = ", reviews_count)
+
+        
+
         print("\n\n")
 
 
         return {
             "grBookID": grBookID,
+            "bookID": bookID,
             "isbn": isbn,
+            "isbn13": isbn13,
             "asin": asin,
             "kindle_asin": kindle_asin,
             "bookID": bookID,
             "title": title,
             "description": description,
             "image_url": image_url,
-            "small_image_url": small_image_urlNode,
-            "author": allAuthors
+            "thumbnail_url": small_image_urlNode,
+            "author": allAuthors,
+            "ratings_count": ratings_count,
+            "reviews_count": reviews_count,
+            "average_score": average_rating,
+            "stars_1": stars_1,
+            "stars_2": stars_2,
+            "stars_3": stars_3,
+            "stars_4": stars_4,
+            "stars_5": stars_5
         }
 
 
