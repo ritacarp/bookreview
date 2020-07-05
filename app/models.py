cf99506a377cc3094d0e4584b6ea0062101314d1
ritacarp <62727366+ritacarp@users.noreply.github.com>
2020-07-05 13:17:22 -0400

    added first and last name to class People

--- app/models.py
+++ app/models.py
@@ -29,6 +29,8 @@ class People(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    first_name = db.Column(db.String, nullable=True)
+    last_name = db.Column(db.String, nullable=True)
     hash = db.Column(db.String, nullable=True)
     comments = db.Column(db.String, nullable=True)
     bookReviews = db.relationship('BookReview', backref='reviewer', lazy=True)
